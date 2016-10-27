12be9535da1ac8de839b74cce82117d803bec75b
杨康 <147180@sh.lianjia.com>
2016-10-27 15:24:30 +0800

    修改日志配置

--- src/main/java/com/eric/redis/configuration/resource/AppPropertyConfigurer.java
+++ src/main/java/com/eric/redis/configuration/resource/AppPropertyConfigurer.java
@@ -2,8 +2,10 @@ package com.eric.redis.configuration.resource;
 import com.eric.redis.common.util.IpUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -18,6 +20,10 @@ public class AppPropertyConfigurer extends PropertyPlaceholderConfigurer {
     protected Properties mergeProperties() throws IOException {
     	Properties superProps = super.mergeProperties();
         superProps.put("env", ConfigurationMap.getEvnDir());
+        //初始化日志
+        File file = new File(ConfigurationMap.getClassRootPath() + superProps.getProperty("env") + "/log4j2.xml");
+        LoggerContext context =(LoggerContext)LogManager.getContext(false);
+        context.setConfigLocation(file.toURI());
         LOGGER.info(">>>>> 运行环境：" + superProps.getProperty("env"));
         LOGGER.info(">>>>> 服务器IP：" + IpUtils.getLocalIP());
         return superProps;
